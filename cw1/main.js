a44588a0a7fd3aecca2dd113308a72b6f0fc1ab1
Vattair <majhu131@gmail.com>
2020-10-15 11:12:10 +0200

    cw1

--- cw1/main.js
+++ cw1/main.js
@@ -1,32 +1,45 @@
 const gallery = document.querySelectorAll('.gallery img');
 const arrowPrev = document.querySelector('.prev');
 const arrowNext = document.querySelector('.next');
+const closeLb = document.querySelector('.closeLb');
 
 arrowNext.addEventListener('click', showNextImg);
 arrowPrev.addEventListener('click', showPrevImg);
+closeLb.addEventListener('click', closeLightbox);
+
+let n = 0;
 
 for(let idx = 0; idx < gallery.length;idx++){
-    const img = gallery[idx];
-    img.addEventListener('click', showLightbox);
+    gallery[idx].id = idx;
+    gallery[idx].addEventListener('click', showLightbox);
 }
 
 function showLightbox(ev) {
-    console.dir(ev.target);
+    n = Number(this.id);
     const lightbox = document.querySelector('.lightbox');
     const img = document.querySelector('.lightbox img');
     img.src = ev.target.src;
     lightbox.classList.add('visible');
 }
-
-function hideLightbox(){
+function closeLightbox() {
     const lightbox = document.querySelector('.lightbox');
     lightbox.classList.remove('visible');
 }
-function showNextImg(ev){
+function showNextImg(){
     const img = document.querySelector('.lightbox img');
-    img.src = ev.target.nextSibling.src;
+    if(n < gallery.length - 1){
+        n += 1;
+    }else{
+        n = 0;
+    }
+    img.src = gallery[n].src;  
 }
-function showPrevImg(ev){
+function showPrevImg(){
     const img = document.querySelector('.lightbox img');
-    img.src = ev.target.previousSibling.src;
+    if(n != 0){
+        n -= 1;
+    }else{
+        n = gallery.length - 1;
+    }
+    img.src = gallery[n].src;  
 }
